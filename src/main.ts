f960fa85290e7df4d6b814e163eebc100fc34884
berdnaski <berdnaski1177@gmail.com>
2025-11-05 00:30:07 -0300

    feat: adding swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,55 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './core/exceptions/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: '*', credentials: true });
+
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
+
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 Server running on port ${process.env.PORT || 3000}`);
+  const config = new DocumentBuilder()
+    .setTitle('Colmeia.io')
+    .setDescription('API de Pagamentos')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Digite o token JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    '/scalar',
+    apiReference({
+      content: document,
+      theme: 'nestjs',
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Server running on port ${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
